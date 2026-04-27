e230a6901d8101420081866653febaea43678918
Fabio <ftellol@iesrafaelalberti.es>
2026-04-27 14:06:39 +0200

    Creando el index y la tabla

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ async function cargarXML() {
     const respuesta = await fetch("xml/recetas.xml");
     const texto = await respuesta.text();
     const parser = DOMParser;
-    const xml = parser.parse(texto);
+    const xml = parser.parse(texto, "application/xml");
     return xml;
 }
 
 function xmlAJson(xml) {
     return Array.from(xml.querySelectorAll("receta")).map(nodo =>({
         codigo: nodo.getAttribute("codigo"),
-        nombre: nodo.querySelector("nombre").textContent || "",
+        nombre: nodo.querySelector("nombre")?.textContent || "",
         categoria: nodo.querySelector("categoria")?.textContent || "",
-        tiempo: nodo.querySelector("tiempo").textContent || "",
-        dificultad: nodo.querySelector("dificultad").textContent || "",
+        tiempo: nodo.querySelector("tiempo")?.textContent || "",
+        dificultad: nodo.querySelector("dificultad")?.textContent || "",
     }))
+}
+
+function pintarJSON (xmlAJson) {
 }
