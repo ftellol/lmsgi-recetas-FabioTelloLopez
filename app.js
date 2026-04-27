f8793958c5f51fe550d3ae5671851694c49229a9
Fabio <ftellol@iesrafaelalberti.es>
2026-04-27 13:35:21 +0200

    Insertada la función xmlAJson, la cual nos ayudara a convertir de xml a Json

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 async function cargarXML() {
     const respuesta = await fetch("xml/recetas.xml");
     const texto = await respuesta.text();
-    const Parser = DOMParser;
-    const xml = Parser.parse(texto);
+    const parser = DOMParser;
+    const xml = parser.parse(texto);
     return xml;
+}
+
+function xmlAJson(xml) {
+    return Array.from(xml.querySelectorAll("receta")).map(nodo =>({
+        codigo: nodo.getAttribute("codigo"),
+        nombre: nodo.querySelector("nombre").textContent || "",
+        categoria: nodo.querySelector("categoria")?.textContent || "",
+        tiempo: nodo.querySelector("tiempo").textContent || "",
+        dificultad: nodo.querySelector("dificultad").textContent || "",
+    }))
 }
